7e4f28bbe21184cb3df6f3f1dc760c6ef3e5a593
Tommy Coleman <tommiilee@gmail.com>
2019-09-19 16:45:45 -0400

    added tests to check response for deleting and finding cats

--- cats/cats.spec.js
+++ cats/cats.spec.js
@@ -13,18 +13,35 @@ describe('cats model', () => {
         expect(cats).toHaveLength(1);
     })
 
+    it('should return the new cat from cats', async () => {
+        await Cats.addCat({name: 'Mrs. Norris'})
+        let cats = await db('cats');
+        expect(cats).toHaveLength(1);
+        const cat = await Cats.findCatByName('Mrs. Norris')
+        console.log(cat)
+        expect(cat.name).toBe('Mrs. Norris')
+    })
+
     it('should delete cat from cats table', async () => {
         await Cats.addCat({name: 'Mr. Bojangles'});
         const cat = await Cats.findCatByName('Mr. Bojangles')
         let cats = await db('cats');
-        console.log(cat)
         expect(cats).toHaveLength(1);
         await Cats.deleteCat(cat.id);
         cats = await db('cats')
         expect(cats).toHaveLength(0);
     })
 
-    it('', async () => {
+    it('should delete cat from table and return 1', async () => {
+        await Cats.addCat({name: 'Mr. Bojangles'});
+        const cat = await Cats.findCatByName('Mr. Bojangles')
+        let cats = await db('cats');
+        expect(cats).toHaveLength(1);
+        await Cats.deleteCat(cat.id).then(res => {
+            expect(res).toBe(1);
+        });
+        cats = await db('cats')
+        expect(cats).toHaveLength(0);
 
     })
 
