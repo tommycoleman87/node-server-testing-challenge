0fbc0f15eadf78b76b164585ae4ce40db44f501d
Tommy Coleman <tommiilee@gmail.com>
2019-09-19 15:20:50 -0400

    added tests to check if the api is up && if the api is returning json

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,5 +6,18 @@ describe('server.js', () => {
         it('should return status 200 ok', async () => {
             await request(server).get('/').then(res => expect(res.status).toBe(500))
         })
+
+        it('should return {api: "up"}', async () => {
+            const res = await request(server).get('/');
+
+            expect(res.body.api).toBe('down')
+            expect(res.body).toBe({api: 'up'})
+        })
+
+        it('should return JSON ', async () => {
+            await request(server).get('/').then(res => {
+                expect(res.type).toMatch(/json/i)
+            })
+        })
     })
 })
